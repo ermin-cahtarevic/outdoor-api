df346fe27314e4ec0ef765338000dc440577be8c
Ermin Cahtarevic <erminc@windowslive.com>
2020-07-15 01:35:54 +0200

    Update Lisiting model - dependant destroy all

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
 
-  has_many :favourites
+  has_many :favourites, dependent: :delete_all
   has_many :favourited_by, through: :favourites, source: :user
 
   validates_presence_of :title, :description
