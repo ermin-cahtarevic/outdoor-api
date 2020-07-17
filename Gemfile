71c69a372861b052c70aac89948b988ef8f30dcf
Ermin Cahtarevic <erminc@windowslive.com>
2020-07-17 20:50:22 +0200

    Add validation and association tests for the User model

--- Gemfile
+++ Gemfile
@@ -33,8 +33,13 @@ gem 'jwt'
 gem 'rubocop', '~>0.81.0'
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails', '~> 3.5'
+end
+
+group :test do
+  gem 'database_cleaner'
+  gem 'factory_bot_rails', '~> 4.0'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
   has_many :favourites
   has_many :favourite_listings, through: :favourites, source: :listing
 
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password
   validates :name, length: { minimum: 3 }
   validates :email, uniqueness: true
-
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 end
