f8823ea32093a050bb81163db8650d302866acfc
Ermin Cahtarevic <erminc@windowslive.com>
2020-07-17 22:46:10 +0200

    Add tests for fetching individual listings

--- spec/requests/listings_spec.rb
+++ spec/requests/listings_spec.rb
@@ -18,4 +18,31 @@ RSpec.describe 'Listings API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /listings/:id' do
+    before { get "/listings/#{listing_id}", params: {}, headers: headers }
+
+    context 'when the record exists' do
+      it 'returns the listing' do
+        expect(json).not_to be_empty
+        expect(json['listing']['id']).to eq(listing_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:listing_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Listing/)
+      end
+    end
+  end
 end
