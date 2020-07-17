6d7a3d0aff4d272b7db9094cb69d0e87a8cb5506
Ermin Cahtarevic <erminc@windowslive.com>
2020-07-17 21:44:53 +0200

    Add validation and association tests for Favourite model

---
+++ spec/factories/listing.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :listing do
+    association :user
+    title { 'Awesome outdoor experience' }
+    description  { 'Awesome outdoor experience description' }
+    rating  { '3.5' }
+    price { '75' }
+    location { 'Somewhere, Far Away' }
+    host { 'John Doe' }
+    host_rating { '2.8' }
+    image { 'some/path/to/an/image' }
+  end
+end
